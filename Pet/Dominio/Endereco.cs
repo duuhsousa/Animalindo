559dde6d5c626eaa8d45e8b4cf30218beb2a6a69
AMANDA BOTELHO DE MORAES <41346828865@jpmorgan.sp>
2017-11-16 21:53:57 -0200

    Endereço

--- Pet/Dominio/Endereco.cs
+++ Pet/Dominio/Endereco.cs
@@ -2,6 +2,24 @@ namespace Dominio
 {
     public class Endereco
     {
-        
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string CEP { get; set; }
+
+        public Endereco(){
+
+        }
+
+        public Endereco(string _Logradouro, int _Numero, string _Bairro, string _Cidade, string _Estado, string _CEP){
+            this.Logradouro = _Logradouro;
+            this.Numero = _Numero;
+            this.Bairro = _Bairro;
+            this.Cidade = _Cidade;
+            this.Estado = _Estado;
+            this.CEP = _CEP;
+        }
     }
 }
