14c565e5b29a9eafa5b36176a6b0544751285a8f
STEVAM DE PASSOS LIMA <47518534819@jpmorgan.sp>
2017-11-16 21:58:21 -0200

    alteração stevam 1.0

--- Pet/Dominio/Animais.cs
+++ Pet/Dominio/Animais.cs
@@ -2,6 +2,12 @@ namespace Pet.Dominio
 {
     public class Animais
     {
-
+        public string Nome { get; set; }
+        public string Nascimento { get; set; }
+        public string Raca { get; set; }
+        public string Cor { get; set; }
+        public string Porte { get; set; }
+        public string Tipo { get; set; }
+        public string ID { get; set; }
     }
 }
