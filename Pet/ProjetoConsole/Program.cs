5d395eef859a7549b9c81a954292ad8b6d55c345
stevamlima <stevamlima99@gmail.com>
2017-11-17 21:00:07 -0200

    alteração stevamlima 2.0

---
+++ Pet/ProjetoConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Repositorio;
+using Dominio;
+
+namespace ProjetoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //============================================LÓGICA DO CADASTRO DE ANIMAIS==========================================================
+            Console.WriteLine("Bem-vindo ao cadastrado de animais");
+            Console.Write("\nDigite qual animal você deseja cadastrar: "); string tipo = Console.ReadLine();
+            Console.Write("Digite a raça do seu animal: "); string raca = Console.ReadLine();
+            Console.Write("Digite a cor do seu animal: "); string cor = Console.ReadLine();
+            Console.Write("Digite o nome do seu animal: "); string nome = Console.ReadLine();
+            Console.Write("Digite a data de nascimento do seu animal: "); DateTime nascimento = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Digite o porte do seu animal: "); string porte = Console.ReadLine();
+
+            string id = string.Empty;
+            Random num = new Random();
+            id = num.Next(0,100).ToString();
+
+            Animais animais = new Animais(tipo,raca,cor,nome,nascimento,porte,id);  //Instancia a classe ANIMAIS pedindo os parâmetros estabelecidos
+            AnimaisRep CadAnimal = new AnimaisRep();
+
+            bool cadastrosucesso = CadAnimal.Cadastrar(animais);
+            if(cadastrosucesso)
+            Console.WriteLine("\nAnimal cadastrado com sucesso.");
+            else
+            Console.WriteLine("\nOcorreu um erro no processo de cadastramento, contacte o ADM.");
+
+            Console.Write("Pesquisar ID do animal:  ");
+            string pesquisa = Console.ReadLine();
+            AnimaisRep PesAnimais = new AnimaisRep();
+            Console.WriteLine(PesAnimais.Consultar(pesquisa));
+            
+
+            //======================================================================================================================================
+
+        }
+    }
+}
+
--- Pet/Repositorio/AnimaisRep.cs
+++ Pet/Repositorio/AnimaisRep.cs
@@ -1,15 +1,77 @@
-namespace Pet.Repositorio
+using System;
+using System.IO;
+using System.Text;
+using Dominio;
+
+namespace Repositorio
 {
     public class AnimaisRep : Dominio.ICrud
     {
-        public string Cadastrar()
+        /// <summary>
+        /// Esta função faz o cadastro dos animais
+        /// </summary>
+        /// <param name="animais"></param>
+        /// <returns>Retorna boolean</returns>
+        public bool Cadastrar(Animais animais)
         {
-            throw new System.NotImplementedException();
-        }
+            bool efetuado = false;
+            StreamWriter arquivo = null;
 
-        public string Consultar()
+            try
+            {
+                arquivo = new StreamWriter("CadastroAnimais.csv", true);
+                arquivo.WriteLine(
+                    animais.ID + ";"+
+                    animais.Nome + ";" + // fazendo um get
+                    animais.Nascimento.ToShortDateString() + ";" + // outro get
+                    animais.Raca + ";" +
+                    animais.Cor + ";" + // Acessamos a classe Cat (categoria) e acessamos o parâmetro Nome
+                    animais.Porte + ";"+
+                    animais.Tipo
+                );
+                efetuado = true;
+            }
+            
+            catch(Exception ex)
+            {
+                throw new Exception("Erro ao tentar gravar o arquivo!"+ex.Message); //exibe uma mensagem avisando que não foi possível gravar no arquivo
+            }
+            finally //fecha o arquivo após o TRY/CATCH
+            {
+                arquivo.Close();
+            }
+            return efetuado;
+        }
+        
+        public string Consultar(string ID)
         {
-            throw new System.NotImplementedException();
+             string resultado = ("ID não encontrada");
+            StreamReader ler = null;
+
+            try
+            {
+                ler = new StreamReader("CadastroAnimais.csv", Encoding.Default); //Encoding.Default serve para utilizar
+                string linha = "";
+                while((linha=ler.ReadLine())!=null)
+                {
+                    string[] dados = linha.Split(';');
+                    if(dados[0] == ID)
+                    {
+                        resultado = linha;
+                        break;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                resultado = ("Erro ao tentar ler o arquivo!"+ex.Message);
+            }
+            finally
+            {
+                ler.Close();
+            }
+
+            return resultado;
         }
     }
 }
